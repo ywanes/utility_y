6c1f9fdbe92c683cac36e8ffd0e6f7acfed1db15
unknown <ywanes@gmail.com>
2020-11-14 15:47:11 -0300

    v0.001.002

--- y/src/y/Y.java
+++ y/src/y/Y.java
@@ -4,18 +4,18 @@ package y;
 // https://github.com/ywanes/utility_y/blob/master/y/src/y/Y.java
 //
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FilterOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
@@ -37,6 +37,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
+
 // java 8
 import java.util.Base64;
 
@@ -54,7 +55,6 @@ import java.util.Base64;
 //	* false} is returned.  Otherwise, equality is determined by using      * the {@link Object#equals equals} method of the first      * argument.      *      * @param a an object      * @param b an object to be compared with {@code a} for equality      * @return {@code true} if the arguments are equal to each other      * and {@code false} otherwise      * @see Object#equals(Object)      */     public static boolean equals(Object a, Object b) {         return (a == b) || (a != null && a.equals(b));     }     /**     * Returns {@code true} if the arguments are deeply equal to each other     * and {@code false} otherwise.     *     * Two {@code null} values are deeply equal.  If both arguments are     * arrays, the algorithm in {@link Arrays#deepEquals(Object[],     * Object[]) Arrays.deepEquals} is used to determine equality.     * Otherwise, equality is determined by using the {@link     * Object#equals equals} method of the first argument.     *     * @param a an object     * @param b an object to be compared with {@code a} for deep equality     * @return {@code true} if the arguments are deeply equal to each other     * and {@code false} otherwise     * @see Arrays#deepEquals(Object[], Object[])     * @see Objects#equals(Object, Object)     */     public static boolean deepEquals(Object a, Object b) {         if (a == b)             return true;         else if (a == null || b == null)             return false;         else{                          System.out.println("Erro, erro na traduzação do java 6");             System.err.println("Erro, erro na traduzação do java 6");             return false;         }              }      /**      * Returns the hash code of a non-{@code null} argument and 0 for      * a {@code null} argument.      *      * @param o an object      * @return the hash code of a non-{@code null} argument and 0 for      * a {@code null} argument      * @see Object#hashCode      */     public static int hashCode(Object o) {         return o != null ? o.hashCode() : 0;     }     /**     * Generates a hash code for a sequence of input values. The hash     * code is generated as if all the input values were placed into an     * array, and that array were hashed by calling {@link     * Arrays#hashCode(Object[])}.     *     * <p>This method is useful for implementing {@link     * Object#hashCode()} on objects containing multiple fields. For     * example, if an object that has three fields, {@code x}, {@code     * y}, and {@code z}, one could write:     *     * <blockquote><pre>     * &#064;Override public int hashCode() {     *     return Objects.hash(x, y, z);     * }     * </pre></blockquote>     *     * <b>Warning: When a single object reference is supplied, the returned     * value does not equal the hash code of that object reference.</b> This     * value can be computed by calling {@link #hashCode(Object)}.     *     * @param values the values to be hashed     * @return a hash value of the sequence of input values     * @see Arrays#hashCode(Object[])     * @see List#hashCode     */     public static int hash(Object... values) {         return Arrays.hashCode(values);     }      /**      * Returns the result of calling {@code toString} for a non-{@code      * null} argument and {@code "null"} for a {@code null} argument.      *      * @param o an object      * @return the result of calling {@code toString} for a non-{@code      * null} argument and {@code "null"} for a {@code null} argument      * @see Object#toString      * @see String#valueOf(Object)      */   
 //	public static String toString(Object o) {         return String.valueOf(o);     }      /**      * Returns the result of calling {@code toString} on the first      * argument if the first argument is not {@code null} and returns      * the second argument otherwise.      *      * @param o an object      * @param nullDefault string to return if the first argument is      *        {@code null}      * @return the result of calling {@code toString} on the first      * argument if it is not {@code null} and the second argument      * otherwise.      * @see Objects#toString(Object)      */     public static String toString(Object o, String nullDefault) {         return (o != null) ? o.toString() : nullDefault;     }      /**      * Returns 0 if the arguments are identical and {@code      * c.compare(a, b)} otherwise.      * Consequently, if both arguments are {@code null} 0      * is returned.      *      * <p>Note that if one of the arguments is {@code null}, a {@code      * NullPointerException} may or may not be thrown depending on      * what ordering policy, if any, the {@link Comparator Comparator}      * chooses to have for {@code null} values.      *      * @param <T> the type of the objects being compared      * @param a an object      * @param b an object to be compared with {@code a}      * @param c the {@code Comparator} to compare the first two arguments      * @return 0 if the arguments are identical and {@code      * c.compare(a, b)} otherwise.      * @see Comparable      * @see Comparator      */     public static <T> int compare(T a, T b, Comparator<? super T> c) {         return (a == b) ? 0 :  c.compare(a, b);     }      /**      * Checks that the specified object reference is not {@code null}. This      * method is designed primarily for doing parameter validation in methods      * and constructors, as demonstrated below:      * <blockquote><pre>      * public Foo(Bar bar) {      *     this.bar = Objects.requireNonNull(bar);      * }      * </pre></blockquote>      *      * @param obj the object reference to check for nullity      * @param <T> the type of the reference      * @return {@code obj} if not {@code null}      * @throws NullPointerException if {@code obj} is {@code null}      */     public static <T> T requireNonNull(T obj) {         if (obj == null)             throw new NullPointerException();         return obj;     }      /**      * Checks that the specified object reference is not {@code null} and      * throws a customized {@link NullPointerException} if it is. This method      * is designed primarily for doing parameter validation in methods and      * constructors with multiple parameters, as demonstrated below:      * <blockquote><pre>      * public Foo(Bar bar, Baz baz) {      *     this.bar = Objects.requireNonNull(bar, "bar must not be null");      *     this.baz = Objects.requireNonNull(baz, "baz must not be null");      * }      * </pre></blockquote>      *      * @param obj     the object reference to check for nullity      * @param message detail message to be used in the event that a {@code      *                NullPointerException} is thrown      * @param <T> the type of the reference      * @return {@code obj} if not {@code null}      * @throws NullPointerException if {@code obj} is {@code null}      */     public static <T> T requireNonNull(T obj, String message) {         if (obj == null)             throw new NullPointerException(message);         return obj;     }      /**      * Returns {@code true} if the provided reference is 
 //	{@code null} otherwise      * returns {@code false}.      *      * @apiNote This method exists to be used as a      * {@link java.util.function.Predicate}, {@code filter(Objects::isNull)}      *      * @param obj a reference to be checked against {@code null}      * @return {@code true} if the provided reference is {@code null} otherwise      * {@code false}      *      * @see java.util.function.Predicate      * @since 1.8      */     public static boolean isNull(Object obj) {         return obj == null;     }      /**      * Returns {@code true} if the provided reference is non-{@code null}      * otherwise returns {@code false}.      *      * @apiNote This method exists to be used as a      * {@link java.util.function.Predicate}, {@code filter(Objects::nonNull)}      *      * @param obj a reference to be checked against {@code null}      * @return {@code true} if the provided reference is non-{@code null}      * otherwise {@code false}      *      * @see java.util.function.Predicate      * @since 1.8      */     public static boolean nonNull(Object obj) {         return obj != null;     }      /**      * Checks that the specified object reference is not {@code null} and      * throws a customized {@link NullPointerException} if it is.      *      * <p>Unlike the method {@link #requireNonNull(Object, String)},      * this method allows creation of the message to be deferred until      * after the null check is made. While this may confer a      * performance advantage in the non-null case, when deciding to      * call this method care should be taken that the costs of      * creating the message supplier are less than the cost of just      * creating the string message directly.      *      * @param obj     the object reference to check for nullity      * @param messageSupplier supplier of the detail message to be      * used in the event that a {@code NullPointerException} is thrown      * @param <T> the type of the reference      * @return {@code obj} if not {@code null}      * @throws NullPointerException if {@code obj} is {@code null}      * @since 1.8      */     public static <T> T requireNonNull(T obj, Supplier<String> messageSupplier) {         if (obj == null)             throw new NullPointerException(messageSupplier.get());         return obj;     } }  interface Supplier<T> {     /**      * Gets a result.      *      * @return a result      */     T get(); } 
-
 
 
 public class Y {    
@@ -2339,48 +2339,7 @@ cat buffer.log
             tabela=tabela.split("\\.")[1];
         }
         
-        String SQL="with"
-        +" FUNCTION func_fix_create_table(p_campo CLOB) RETURN CLOB AS "
-        +"   vCampo     CLOB;"
-        +"   vResultado CLOB;"
-        +"   vC         VARCHAR2(2);"
-        +"   vStart     VARCHAR2(1);"
-        +"   vContador  number;"
-        +"   "
-        +" BEGIN"
-        +"   vCampo := p_campo;"
-        +"   vStart := 'N';"
-        +"   vResultado := '';"
-        +"   vContador := 0;"
-        +" "
-        +"   FOR i IN 1..LENGTH(vCampo)"
-        +"   LOOP    "
-        +"     vC := substr(vCampo,i,1);"
-        +"     "
-        +"     IF ( vC = '(' OR vC = 'C' OR vC = 'c' ) THEN"
-        +"       vStart := 'S';"
-        +"     END IF;"
-        +"     "
-        +"     IF ( vC = '(' ) THEN"
-        +"       vContador := vContador + 1;"
-        +"     END IF;"
-        +"     "
-        +"     IF ( vStart = 'S' ) THEN"
-        +"       vResultado := vResultado || vC;"
-        +"     END IF;"
-        +"     "
-        +"     IF ( vC = ')' ) THEN"
-        +"       vContador := vContador - 1;"
-        +"       IF ( vContador = 0 ) THEN          "
-        +"         EXIT;"
-        +"       END IF;"
-        +"     END IF;"
-        +"   END LOOP;"
-        +"   "
-        +"   return vResultado || ';';"
-        +"   "
-        +" END func_fix_create_table;"
-        +" select func_fix_create_table(dbms_metadata.get_ddl('TABLE',UPPER('"+tabela+"'),UPPER('"+schema+"'))) TXT from dual";
+        String SQL=lendo_arquivo_pacote(getClass().getResourceAsStream("/y/sql_get_ddl_createtable")).replace("[TABELA]",tabela).replace("[SCHEMA]",schema);
 
         try{    
             String retorno="";
@@ -2477,5 +2436,15 @@ cat buffer.log
         }catch (Exception e){}
         return result;
     }
-    
+
+    public void MetodoGaranteAPermanenciaDeAlgunsImportsJava()
+    {
+        FilterOutputStream a;
+        IOException b;
+        OutputStream c;
+        ByteBuffer d;
+        Charset e;
+        Arrays f;
+        Comparator g;
+    }
 }
