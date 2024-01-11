ef81dc9dd7c4aa97686ae5581a9c16e36b93ece9
unknown <ywanes@gmail.com>
2024-01-11 13:29:07 -0300

    v0.001.003.002

--- y/src/Y.java
+++ y/src/Y.java
@@ -12971,12 +12971,15 @@ class XML extends Util{
 /* class texto_longo */                 back="";
 /* class texto_longo */             }
 /* class texto_longo */         }else{
-/* class texto_longo */             String txt="";
+/* class texto_longo */             String trs="";
+/* class texto_longo */             String curl="";
 /* class texto_longo */             String h1="<br>";
 /* class texto_longo */             if ( !path.equals(".") )
 /* class texto_longo */                 h1="<a href=\"/\"><svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"> <g clip-path=\"url(#clip0)\"> <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M12.5882 3.66429C12.2376 3.40927 11.7625 3.40927 11.4119 3.66429L5.00007 8.32743V20C5.00007 20.5523 5.44779 21 6.00007 21H8.00007V15C8.00007 13.3432 9.34322 12 11.0001 12H13.0001C14.6569 12 16.0001 13.3432 16.0001 15V21H18.0001C18.5524 21 19.0001 20.5523 19.0001 20V8.32743L12.5882 3.66429ZM21.0001 9.78198L22.4119 10.8088C22.8586 11.1336 23.484 11.0349 23.8088 10.5882C24.1336 10.1415 24.0349 9.51613 23.5882 9.19129L13.7646 2.04681C12.7126 1.28176 11.2875 1.28176 10.2356 2.04681L0.411899 9.19129C-0.0347537 9.51613 -0.133504 10.1415 0.191334 10.5882C0.516173 11.0349 1.14159 11.1336 1.58824 10.8088L3.00007 9.78198V20C3.00007 21.6569 4.34322 23 6.00007 23H18.0001C19.6569 23 21.0001 21.6569 21.0001 20V9.78198ZM14.0001 21V15C14.0001 14.4477 13.5524 14 13.0001 14H11.0001C10.4478 14 10.0001 14.4477 10.0001 15V21H14.0001Z\" fill=\"#293644\"></path> </g> <defs> <clipPath id=\"clip0\"> <rect width=\"24\" height=\"24\" fill=\"white\"></rect> </clipPath> </defs> </svg></a><br><h1 style=\"color: white;\">&nbsp;/" + path + "</h1><br>";
-/* class texto_longo */             for ( int i=0;i<elementos.size();i++ )
-/* class texto_longo */                 txt += "<tr><td style=\"width: 1570px; display: inline-block; cursor: pointer; color: white; font-size: 24px;\" onclick=\"window.location.replace('" + prefix + encodeUrl(elementos.get(i)) + "')\">" + elementos.get(i) + "</td></tr>\n";
+/* class texto_longo */             for ( int i=0;i<elementos.size();i++ ){
+/* class texto_longo */                 trs += "<tr><td style=\"width: 1570px; display: inline-block; cursor: pointer; color: white; font-size: 24px;\" onclick=\"window.location.replace('" + prefix + encodeUrl(elementos.get(i)) + "')\">" + elementos.get(i) + "</td></tr>\n";
+/* class texto_longo */                 curl += "curl \"" + (prefix + encodeUrl(elementos.get(i))).replace("/id/", "http://203.cloudns.cl:8895/") + "\" > \"" + elementos.get(i) + "\"\n";
+/* class texto_longo */             }
 /* class texto_longo */             return "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n" +
 /* class texto_longo */             "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
 /* class texto_longo */             "<body style=\"background-color: rgb(0, 0, 0);\">" + 
@@ -12985,7 +12988,10 @@ class XML extends Util{
 /* class texto_longo */             "<style>.bordered {border: solid #ccc 3px;border-radius: 6px;}.bordered td, .bordered th {border-left: 2px solid #ccc;border-top: 2px solid #ccc;padding: 10px;}</style>\n" + 
 /* class texto_longo */             h1 + 
 /* class texto_longo */             "<table id='tablebase' class='bordered' style='font-family:Verdana,sans-serif;font-size:10px;border-spacing: 0;'>\n" +
-/* class texto_longo */             txt +
+/* class texto_longo */             "<!--\n" + 
+/* class texto_longo */             curl +
+/* class texto_longo */             "-->\n" + 
+/* class texto_longo */             trs +
 /* class texto_longo */             "</table></body></html>";
 /* class texto_longo */         }
 /* class texto_longo */         String id_display=decodeUrl(id);
