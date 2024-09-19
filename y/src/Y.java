6924b4df4cca31071eef0bbb0876d6e55c7ed855
unknown <ywanes@gmail.com>
2024-09-19 01:03:15 -0300

    v0.001.003.002

--- y/src/Y.java
+++ y/src/Y.java
@@ -5175,7 +5175,7 @@ cat buffer.log
         if ( overflix_multi != null ){
             overflix_multi.wait_numeroDeTrabalhoIgualOuMenor(0);            
             if ( overflix_busca_skip )
-                System.out.println("token com problema, volte daqui 30 minutos.");
+                System.out.println("token indisponivel no momento, volte daqui 30 minutos.");
             sleepSeconds(2);
             System.exit(0);
         }
